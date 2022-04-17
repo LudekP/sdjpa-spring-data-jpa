90b8c3e304a65ff3200229b6e1e0210237960bbe
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-04-17 09:37:46 +0200

    Spring JPA queries

---
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
@@ -0,0 +1,42 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import guru.springframework.jdbc.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class BookDaoImpl implements BookDao {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book getById(Long id) {
+        return bookRepository.getById(id);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        return bookRepository.findByTitle(title).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Book saveNewBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Transactional
+    @Override
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
